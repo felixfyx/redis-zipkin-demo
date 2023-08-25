91d4dd4773e3446dbf280d8d12425841ae0ea664
FelixF <felixfyx@gmail.com>
2023-08-25 10:08:58 +0800

    Added zipkin component to this project

--- redis_publisher/src/go.mod
+++ redis_publisher/src/go.mod
@@ -4,15 +4,21 @@ go 1.20
 
 require (
 	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/openzipkin/zipkin-go v0.4.2
 	github.com/redis/go-redis/v9 v9.1.0
+	go.opentelemetry.io/otel v1.16.1-0.20230818142432-f15ae160c4ac
+	go.opentelemetry.io/otel/exporters/zipkin v1.16.0
+	go.opentelemetry.io/otel/sdk v1.16.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/klauspost/compress v1.16.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
@@ -20,5 +26,8 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
+	google.golang.org/grpc v1.57.0 // indirect
 )
--- redis_publisher/src/main.go
+++ redis_publisher/src/main.go
@@ -1,5 +1,6 @@
 /*
-This is based off the redis demo I've made here: https://github.com/felixfyx/go-redis-demo
+This is based off the redis demo I've made here:
+https://github.com/felixfyx/go-redis-demo
 
 Based on the server
 */
@@ -9,10 +10,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
+	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/trace"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 type LogText struct {
@@ -20,21 +32,134 @@ type LogText struct {
 	Message string `json:"message"`
 }
 
+/*
+Note as of 24/8/2023:
+
+Sometimes information about a runtime environment can change dynamically or be
+delayed from startup. Instead of continuously recreating and distributing a
+TracerProvider with an immutable Resource or delaying the startup of your
+application on a slow-loading piece of information, annotate the created spans
+dynamically using a SpanProcessor.
+*/
+
+var (
+	// owner represents the owner of the application. In this example it is
+	// stored as a simple string, but in real-world use this may be the
+	// response to an asynchronous request.
+	owner    = "unknown"
+	ownerKey = attribute.Key("owner")
+)
+
+// Annotator is a SpanProcessor that adds attributes to all started spans.
+type Annotator struct {
+	// AttrsFunc is called when a span is started. The attributes it returns
+	// are set on the Span being started.
+	AttrsFunc func() []attribute.KeyValue
+}
+
+func (a Annotator) OnStart(_ context.Context, s sdktrace.ReadWriteSpan) {
+	s.SetAttributes(a.AttrsFunc()...)
+}
+func (a Annotator) Shutdown(context.Context) error   { return nil }
+func (a Annotator) ForceFlush(context.Context) error { return nil }
+func (a Annotator) OnEnd(s sdktrace.ReadOnlySpan) {
+	attr := s.Attributes()[0]
+	fmt.Printf("%s: %s\n", attr.Key, attr.Value.AsString())
+}
+
+// Logger to be used with zipkin
+var logger = log.New(os.Stderr, "zipkin-example",
+	log.Ldate|log.Ltime|log.Llongfile)
+
+// initTracer creates a new trace provider instance and registers it as global trace provider.
+func initTracer(url string) (func(context.Context) error, error) {
+	// Create Zipkin Exporter and install it as a global tracer.
+	//
+	// For demoing purposes, always sample. In a production application, you should
+	// configure the sampler to a trace.ParentBased(trace.TraceIDRatioBased) set at the desired
+	// ratio.
+	exporter, err := zipkin.New(
+		url,
+		zipkin.WithLogger(logger),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	batcher := sdktrace.NewBatchSpanProcessor(exporter)
+
+	// annotations
+	a := Annotator{
+		AttrsFunc: func() []attribute.KeyValue {
+			return []attribute.KeyValue{ownerKey.String(owner)}
+		},
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSpanProcessor(batcher),
+		sdktrace.WithSpanProcessor(a),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("publisher-metrics"),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+
+	return tp.Shutdown, nil
+}
+
 func main() {
+	// Setup redis related stuff
 	// Read config off env to get redis host and port number
 	var host = os.Getenv("redis_host")
 	var port = os.Getenv("redis_port")
 
-	fmt.Println("--- Connecting to Redis Publisher ---")
+	fmt.Println("--- Setting up Redis Publisher stuff ---")
 	var redisClient = redis.NewClient(&redis.Options{
 		Addr: host + ":" + port,
 	})
 
-	fmt.Println("Running Publisher...")
-	app := fiber.New()
-	var ctx = context.Background()
+	fmt.Println("--- Setting up Zipkin stuff ---")
+	// Setup zipkin related stuff
+	var zHost = os.Getenv("zipkin_host")
+	var zPort = os.Getenv("zipkin_port")
+	url := "http://" + zHost + ":" + zPort + "/api/v2/spans"
 
+	app := fiber.New()
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	// Initializing tracer
+	shutdown, err := initTracer(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown TracerProvider: %w", err)
+		}
+	}()
+	fmt.Println("Initialize tracer to: " + url)
+
+	// Creating a unique tracer
+	tracer := otel.GetTracerProvider().Tracer("publisher-tracer")
+	ctx, span := tracer.Start(ctx, "redisPublisher", trace.WithSpanKind(trace.SpanKindServer))
+	defer span.End() // Attempt to end the span during cleanup
+
+	fmt.Println("Running Publisher...")
+	// Endpoint stuff
 	app.Post("/", func(c *fiber.Ctx) error {
+		// Creating a new tracer for calculating how long it takes for this
+		// function to clear
+		tr := otel.GetTracerProvider().Tracer("publisher-post")
+		owner = "redis"
+		_, span := tr.Start(ctx, "post-log-event")
+		// HACK: Probably a hacky way of getting things done but at least
+		// it does get things done.
+		// This will end the span that is post-event from the publisher-post
+		// tracer
+		defer span.End()
+
 		logtext := new(LogText)
 
 		if err := c.BodyParser(logtext); err != nil {
@@ -46,13 +171,15 @@ func main() {
 			panic(err)
 		}
 
-		if err := redisClient.Publish(ctx, "send-log-data", payload).Err(); err != nil {
+		err = redisClient.Publish(ctx, "send-log-data", payload).Err()
+		if err != nil {
 			panic(err)
 		}
 
-		return c.SendStatus(200)
+		c.WriteString("Sent payload: " + string(payload))
+
+		return c.SendStatus(http.StatusOK)
 	})
 
-	// HACK: Hardcoded for now..
 	app.Listen(":8080")
 }
